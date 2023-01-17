d662285075efb4d1320947ac00ba4b27717cb4db
Christian Lozano <christianlozano729@gmail.com>
2023-01-17 09:13:28 -0600

    random

--- flask_mysql/validation/work/New-Recipes/flask_app/controllers/users.py
+++ flask_mysql/validation/work/New-Recipes/flask_app/controllers/users.py
@@ -38,10 +38,10 @@ def login():
 
     if not user:
         flash("Invalid Email", "login")
-        return redirect('/')
+        return redirect('/logging')
     if not bcrypt.check_password_hash(user.password, request.form['password']):
         flash("Invalid Password", "login")
-        return redirect('/')
+        return redirect('/logging')
     session['user_id'] = user.id
     return redirect('/dashboard')
 
